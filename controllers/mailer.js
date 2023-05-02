968ae0e4fe2c54d3aee9d34b45159b02dc40579f
Kevin Han <yhan893071@gmail.com>
2023-05-02 00:29:47 -0400

    first commit

---
+++ controllers/mailer.js
@@ -0,0 +1,65 @@
+import nodemailer from "nodemailer";
+import Mailgen from "mailgen";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+let MailGenerator = new Mailgen({
+  theme: "default",
+  product: {
+    name: "Mailgen",
+    link: "https://mailgen.js/",
+  },
+});
+
+/** POST: http://localhost:8080/api/registerMail 
+ * @param: {
+  "username" : "example123",
+  "userEmail" : "admin123",
+  "text" : "",
+  "subject" : "",
+}
+*/
+export const registerMail = async (req, res) => {
+  const { username, userEmail, text, subject } = req.body;
+
+  // body of the email
+  var response = {
+    body: {
+      name: username,
+      intro:
+        text ||
+        "Welcome to Daily Tuition! We're very excited to have you on board.",
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+
+  var emailBody = MailGenerator.generate(response);
+
+  let message = {
+    from: process.env.EMAIL,
+    to: userEmail,
+    subject: subject || "Signup Successful",
+    html: emailBody,
+  };
+
+  // send mail
+  transporter
+    .sendMail(message)
+    .then(() => {
+      return res
+        .status(201)
+        .send({ msg: "You should receive an email from us." });
+    })
+    .catch((error) => res.status(500).send({ error }));
+};
